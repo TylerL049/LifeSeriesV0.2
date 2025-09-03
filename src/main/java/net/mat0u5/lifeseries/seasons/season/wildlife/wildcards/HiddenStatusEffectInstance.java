377e73aea2d9fb4e8d905a859ee7bd644fbc81a1
TylerL049 <tylerjay04@icloud.com>
2025-09-03 20:45:51 +0100

    added registry import

--- src/main/java/net/mat0u5/lifeseries/seasons/season/wildlife/wildcards/HiddenStatusEffectInstance.java
+++ src/main/java/net/mat0u5/lifeseries/seasons/season/wildlife/wildcards/HiddenStatusEffectInstance.java
@@ -1,11 +1,12 @@
 package net.mat0u5.lifeseries.seasons.season.wildlife.wildcards;
 
+import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryEntry;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectInstance;
 
 public class HiddenStatusEffectInstance extends StatusEffectInstance {
-    public HiddenStatusEffectInstance(RegistryEntry<StatusEffect> type, int duration, int amplifier) {
-        super(type, duration, amplifier, false, false, false);
+    public HiddenStatusEffectInstance(StatusEffect type, int duration, int amplifier) {
+        super(Registry.STATUS_EFFECT.getEntry(type), duration, amplifier, false, false, false);
     }
 }
