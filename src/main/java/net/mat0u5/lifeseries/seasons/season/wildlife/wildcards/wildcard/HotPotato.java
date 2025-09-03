6ed32ac4edfdefa3b45ad45e18cd50bf518e9c22
TylerL049 <tylerjay04@icloud.com>
2025-09-03 19:14:44 +0100

    stupid missing letters

--- src/main/java/net/mat0u5/lifeseries/seasons/season/wildlife/wildcards/wildcard/HotPotato.java
+++ src/main/java/net/mat0u5/lifeseries/seasons/season/wildlife/wildcards/wildcard/HotPotato.java
@@ -1,4 +1,4 @@
-﻿package net.mat0u5.lifeseries.seasons.season.wildlife.wildcards.wildcard;
+﻿papackage net.mat0u5.lifeseries.seasons.season.wildlife.wildcards.wildcard;
 
 import net.mat0u5.lifeseries.seasons.season.wildlife.wildcards.Wildcard;
 import net.mat0u5.lifeseries.seasons.season.wildlife.wildcards.Wildcards;
