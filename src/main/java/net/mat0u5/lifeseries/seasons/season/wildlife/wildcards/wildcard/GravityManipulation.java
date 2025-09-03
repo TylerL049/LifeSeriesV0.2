82dbdf342b55fde47c622024879665a900ccad64
TylerL049 <tylerjay04@icloud.com>
2025-09-03 20:52:39 +0100

    removed hidden potion effect for gravity. Will attempt tomorrow

--- src/main/java/net/mat0u5/lifeseries/seasons/season/wildlife/wildcards/wildcard/GravityManipulation.java
+++ src/main/java/net/mat0u5/lifeseries/seasons/season/wildlife/wildcards/wildcard/GravityManipulation.java
@@ -3,8 +3,8 @@ package net.mat0u5.lifeseries.seasons.season.wildlife.wildcards.wildcard;
 import net.mat0u5.lifeseries.seasons.season.wildlife.wildcards.Wildcard;
 import net.mat0u5.lifeseries.seasons.season.wildlife.wildcards.Wildcards;
 import net.mat0u5.lifeseries.utils.player.PlayerUtils;
+import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
-import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.server.network.ServerPlayerEntity;
 
 import java.util.Random;
@@ -12,7 +12,6 @@ import java.util.Random;
 public class GravityManipulation extends Wildcard {
 
     private static final int TICKS_PER_SECOND = 20;
-    private static final int INITIAL_JUMP_DELAY = 60 * TICKS_PER_SECOND;
     private static final int JUMP_BUILDUP_TICKS = 45 * 60 * TICKS_PER_SECOND;
     private static final int LEVITATION_DURATION_START = 5 * TICKS_PER_SECOND;
     private static final int LEVITATION_DURATION_INCREMENT = 5 * TICKS_PER_SECOND;
@@ -34,6 +33,7 @@ public class GravityManipulation extends Wildcard {
     private boolean levitating = false;
     private int currentLevitationDuration = LEVITATION_DURATION_START;
     private boolean firstLevitation = true;
+    private static final int INITIAL_JUMP_DELAY = 60 * TICKS_PER_SECOND;
 
     @Override
     public Wildcards getType() {
@@ -51,16 +51,16 @@ public class GravityManipulation extends Wildcard {
                 if (tickCounter > INITIAL_JUMP_DELAY) {
                     double progress = (double) (tickCounter - INITIAL_JUMP_DELAY) / (JUMP_BUILDUP_TICKS - INITIAL_JUMP_DELAY);
                     int jumpLevel = 1 + (int) Math.floor(progress * (MAX_JUMP_BUILDUP - 1));
-                    player.addStatusEffect(new HiddenStatusEffectInstance(StatusEffects.JUMP_BOOST, 40, jumpLevel - 1));
+                    player.addStatusEffect(new StatusEffectInstance(StatusEffects.JUMP_BOOST, 40, jumpLevel - 1, false, false, false));
                 }
             } else {
                 if (levitating) {
                     if (tickCounter < phaseStartTick + currentLevitationDuration) {
-                        player.addStatusEffect(new HiddenStatusEffectInstance(StatusEffects.LEVITATION, 40, levitationLevel - 1));
+                        player.addStatusEffect(new StatusEffectInstance(StatusEffects.LEVITATION, 40, levitationLevel - 1, false, false, false));
                     } else {
                         levitating = false;
                         int jumpLevel = 3 + random.nextInt(MAX_JUMP_BUILDUP - 2);
-                        player.addStatusEffect(new HiddenStatusEffectInstance(StatusEffects.JUMP_BOOST, 40, jumpLevel - 1));
+                        player.addStatusEffect(new StatusEffectInstance(StatusEffects.JUMP_BOOST, 40, jumpLevel - 1, false, false, false));
                         phaseStartTick = tickCounter;
 
                         if (firstLevitation) {
@@ -77,7 +77,7 @@ public class GravityManipulation extends Wildcard {
                             ? player.getStatusEffect(StatusEffects.JUMP_BOOST).getAmplifier()
                             : MAX_JUMP_BUILDUP - 1;
 
-                    player.addStatusEffect(new HiddenStatusEffectInstance(StatusEffects.JUMP_BOOST, 40, currentJump));
+                    player.addStatusEffect(new StatusEffectInstance(StatusEffects.JUMP_BOOST, 40, currentJump, false, false, false));
 
                     if (nextEventTick == -1) {
                         nextEventTick = tickCounter + FIRST_RANDOM_DELAY_MIN + random.nextInt(FIRST_RANDOM_DELAY_MAX - FIRST_RANDOM_DELAY_MIN + 1);
